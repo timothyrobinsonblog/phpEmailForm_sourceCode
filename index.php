973a29c72e6e79c2aabc58169daf357dfec80a48
timothyrobinsonblog <53365029+timothyrobinsonblog@users.noreply.github.com>
2022-05-01 22:50:18 +1000

    capslock

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
           <span class="circle one"></span>
           <span class="circle two"></span>
 
-          <form action="index.php" method="post" autocomplete="off">
+          <form action="index.php" method="POST" autocomplete="off">
             <h3 class="title">Contact us</h3>
             <div class="input-container">
               <input type="text" name="name" class="input" />
